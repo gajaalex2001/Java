bb7b13765315e94513caf605d92e4cdcdac7da44
gajaalex2001 <100204048+gajaalex2001@users.noreply.github.com>
2022-04-13 21:25:49 +0300

    Add files via upload

---
+++ ProiectNetworking/TriatlonServices/src/main/java/services/TriatlonException.java
@@ -0,0 +1,9 @@
+package services;
+
+public class TriatlonException extends Exception{
+    public TriatlonException(){}
+
+    public TriatlonException(String message){super(message);}
+
+    public TriatlonException(String message, Throwable cause) {super(message, cause);}
+}
